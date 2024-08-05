09fe334dafb3e3591a9c9c9d6103be0625d6971c
Dane Guevara <rainerdane@gmail.com>
2024-08-05 14:54:57 +0000

    update nvim

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,45 +1,45 @@
 local harpoon = require('harpoon')
-local action = require('telescope.actions')
+-- local action = require('telescope.actions')
 
 -- REQUIRED
 harpoon:setup({})
 -- REQUIRED
 
 -- basic telescope configuration
-local config = require('telescope.config').values
-local function toggle_telescope(harpoon_files)
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
+-- local config = require('telescope.config').values
+-- local function toggle_telescope(harpoon_files)
+--   local file_paths = {}
+--   for _, item in ipairs(harpoon_files.items) do
+--     table.insert(file_paths, item.value)
+--   end
 
-  require('telescope.pickers').new({
-    attach_mappings = function(prompt_bufnr, map)
-      map('i', '<c-e>', 'close')
-      map('n', '<c-e>', 'close')
-      map('i', '<c-a>', function()
-        action.close(prompt_bufnr)
+--   require('telescope.pickers').new({
+--     attach_mappings = function(prompt_bufnr, map)
+--       map('i', '<c-e>', 'close')
+--       map('n', '<c-e>', 'close')
+--       map('i', '<c-a>', function()
+--         action.close(prompt_bufnr)
 
-        harpoon.ui:toggle_quick_menu(harpoon:list(), { title = ' 󰈸 󰈸 fires 󰈸 󰈸 ' })
-      end)
+--         harpoon.ui:toggle_quick_menu(harpoon:list(), { title = ' 󰈸 󰈸 fires 󰈸 󰈸 ' })
+--       end)
 
-      return true
-    end,
-  }, {
-    prompt_title = '󰈸 this is fine 󰈸',
-    sorting_strategy = 'ascending',
-    finder = require('telescope.finders').new_table({
-      results = file_paths,
-    }),
-    previewer = config.file_previewer({}),
-    sorter = config.generic_sorter({}),
-  }):find()
-end
+--       return true
+--     end,
+--   }, {
+--     prompt_title = '󰈸 this is fine 󰈸',
+--     sorting_strategy = 'ascending',
+--     finder = require('telescope.finders').new_table({
+--       results = file_paths,
+--     }),
+--     previewer = config.file_previewer({}),
+--     sorter = config.generic_sorter({}),
+--   }):find()
+-- end
 
 vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
-vim.keymap.set('n', '<c-e>', function()
-  toggle_telescope(harpoon:list())
-end, { desc = '󰈸 this is fine 󰈸' })
+-- vim.keymap.set('n', '<c-e>', function()
+--   toggle_telescope(harpoon:list())
+-- end, { desc = '󰈸 this is fine 󰈸' })
 
 vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
 vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
