1c7c5c810ef4a9253b5e985ae538425a6d89e8e7
Dane Guevara <rainerdane@gmail.com>
2025-02-06 20:36:06 +0000

    updates

--- nvim/after/plugin/noice.lua
+++ nvim/after/plugin/noice.lua
@@ -25,7 +25,7 @@ noice.setup({
   },
   popupmenu = {
     enabled = true, -- enables the Noice popupmenu UI
-    ---@type 'nui'|'cmp'
+    ---@type "nui"|"cmp"
     backend = "nui", -- backend to use to show regular cmdline completions
     -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
     kind_icons = {}, -- set to `false` to disable icons
